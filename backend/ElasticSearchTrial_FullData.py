15df205186e522c7992decb1fac6839bcd11bc61
shweta-24 <shwetamisra24@gmail.com>
2022-05-05 22:14:49 +0200

    Backend code with flask server

--- backend/ElasticSearchTrial_FullData.py
+++ backend/ElasticSearchTrial_FullData.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[32]:
+# In[108]:
 
 
 from elasticsearch import Elasticsearch
@@ -11,14 +11,20 @@ import os
 import json
 import xml.etree.ElementTree as ET
 from tfidf import TfIdf
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS, cross_origin
 
-
-# In[33]:
-
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+api = Api(app)
 
 transcript_dir = 'E:\Masters\Search Engines and Information Retrieval Systems\Project\podcasts-no-audio-13GB\spotify-podcasts-2020\podcasts-transcripts'
 rss_dir = 'E:\Masters\Search Engines and Information Retrieval Systems\Project\podcasts-no-audio-13GB\spotify-podcasts-2020\show-rss'
 
+showhash2name = {}
+episodehash2name ={}
 trans2ID = {}
 transcriptLst = []
 idx = 0
@@ -29,88 +35,6 @@ idx = 0
 # e.g. res= [(query, "show_01DbRiALDPdvZdoiY8yQL6", "5fG4VlWnWwzAt6mSs0H7lY", episcore)]
 
 table = TfIdf()
-
-class transcript():
-    def __init__(self, id, score, words):
-        self.id = id
-        self.score = score
-        self.words = words
-
-def getClips(res, n_minute, n_clips):
-    result = []
-    episode_frequency = {}
-    for (qr, show, episode, score) in res:
-        episode_frequency[episode] = 0
-        subdir1, subdir2 = show.split('_')[1][0], show.split('_')[1][1]
-        transcript_json_dir = os.path.join(transcript_dir, subdir1, subdir2, show, episode+'.json')
-        rss_xml_dir = os.path.join(rss_dir, subdir1, subdir2, show+'.xml')
-        rss_tree = ET.parse(rss_xml_dir)
-        rss_root = rss_tree.getroot()
-        rss_url = getUrlFromXml(rss_root)
-        with open(transcript_json_dir, ) as f:
-            transcript_json = json.load(f)
-            
-            addDocuments(transcript_json, score, show, episode, rss_url)
-    getRelevantClips(qr)
-    
-    rankingTranscript()
-    # for element in transcriptLst:
-    #     print(element.id, element.score)
-    for element in transcriptLst:
-        show, episode, rss_url, start, end = trans2ID[element.id]
-        cnt = element.id
-        if element.score == 0:
-            continue
-        while cnt + 1 < idx:
-            if trans2ID[cnt + 1][1] != episode or (trans2ID[cnt + 1][-1] - start) > n_minute or episode_frequency[episode] > n_clips:
-                break
-            end = trans2ID[cnt + 1][-1]
-            cnt += 1
-        result.append((show, episode, rss_url, start, end))
-        episode_frequency[episode] += 1
-    return result
-
-def getUrlFromXml(root):
-    item = root[0].find('item')
-    url = item.find('enclosure')
-    return url.attrib['url']
-
-def rankingTranscript():
-    transcriptLst.sort(key=lambda x: x.score, reverse=True)
-
-def addDocuments(data, score, show, episode, rss_url):
-    start = 0
-    end = 0
-    global idx
-    alternatives = data['results']
-    for alternative in alternatives:
-        transcripts_data = alternative['alternatives']
-        if transcripts_data == [{}]:
-            continue
-        for ele in transcripts_data:
-            if 'transcript' not in ele.keys():
-                continue
-            trans = ele['transcript']
-            start = float(ele['words'][0]['startTime'].rstrip('s'))
-            end = float(ele['words'][-1]['endTime'].rstrip('s'))
-            tmp = transcript(idx, score, trans.split())
-            # s = tmp.calTFIDF(qr)
-            # if s != 0:
-            transcriptLst.append(tmp)
-            table.add_document(idx, trans.upper().split())
-            trans2ID[idx] = (show, episode, rss_url, start, end)
-            
-            idx += 1
-    # print(table.corpus_dict)
-
-def getRelevantClips(qr, epi_w=0.4):
-    table2 = table.similarities(qr)
-    for i in range(idx):
-        transcriptLst[i].score = epi_w * transcriptLst[i].score + (1 - epi_w) * table2[i][1]
-    # print(table2)
-
-
-# In[34]:
 
 
 class ElasticSearchClient:
@@ -127,6 +51,17 @@ class ElasticSearchClient:
         #self.client.indices.delete(index='spotify-metadata', ignore=[400, 404])   
         if self.client.indices.exists(index = "spotify-metadata"):
             print("Index already exists!")
+            tsv_file = open("podcasts-no-audio-13GB\spotify-podcasts-2020\metadata.tsv", encoding="utf8")
+            read_tsv = csv.reader(tsv_file, delimiter="\t")
+            header = True
+            for row in read_tsv:
+                if header:
+                    header = False
+                    continue
+                showhash2name[row[10]] =  row[1]
+                episodehash2name[row[11]] = row[7]
+            tsv_file.close()
+            #print(showhash2name)
         else:
             print("Creating the metadata index...")
             response = self.create_index_metadata(indexname = "spotify-metadata")
@@ -216,30 +151,145 @@ class ElasticSearchClient:
                     }
                 }
             }
-        res = self.client.search(index="spotify-metadata", body=query_body, size=100)
+        res = self.client.search(index="spotify-metadata", body=query_body, size=10)
         print(len(res['hits']['hits']))
         result = []
         for doc in res['hits']['hits']:
             result.append((query.split(), doc['_source']['show_filename_prefix'], doc['_source']['episode_filename_prefix'], doc['_score']))
         return result
 
+es = ElasticSearchClient()
+        
+class Search(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('query', required=True)
+        parser.add_argument('fields', required=True)
+        args = parser.parse_args()
+        
+        start = time.time()
+        results = es.search(args['query'], args['fields'].split('|'))
+        result = getClips(results, 120, 10)
+        print("Time to search: ",(time.time()-start))
+        print(result[0])
+        return result, 200
 
-# In[35]:
+
+    
+    
+# In[109]:
 
 
-def main():
-    es = ElasticSearchClient()
-    start = time.time()
-    results = es.search("JESUS")
-    result = getClips(results, 120, 2)
-    print("Time to search: ",(time.time()-start))
-    print(len(result))
+class transcript():
+    def __init__(self, id, score, words, content):
+        self.id = id
+        self.score = score
+        self.words = words
+        self.content = content
+
+def getClips(res, n_minute, n_clips):
+    result = []
+    episode_frequency = {}
+    for (qr, show, episode, score) in res:
+        episode_frequency[episode] = 0
+        subdir1, subdir2 = show.split('_')[1][0], show.split('_')[1][1]
+        transcript_json_dir = os.path.join(transcript_dir, subdir1, subdir2, show, episode+'.json')
+        rss_xml_dir = os.path.join(rss_dir, subdir1, subdir2, show+'.xml')
+        rss_tree = ET.parse(rss_xml_dir)
+        rss_root = rss_tree.getroot()
+        try:
+            rss_url = getUrlFromXml(rss_root)
+        except Exception as e:
+            #print(rss_xml_dir)
+            continue
+        with open(transcript_json_dir, ) as f:
+            transcript_json = json.load(f)
+            
+            addDocuments(transcript_json, score, show, episode, rss_url)
+    getRelevantClips(qr)
+    
+    rankingTranscript()
+    # for element in transcriptLst:
+    #     print(element.id, element.score)
+    for element in transcriptLst:
+        show, episode, rss_url, start, end, trans = trans2ID[element.id]
+        cnt = element.id
+        if element.score == 0 or episode_frequency[episode] >= n_clips:
+            continue
+        final_trans = trans    
+        while cnt + 1 < idx:
+            if trans2ID[cnt + 1][1] != episode or (trans2ID[cnt + 1][-2] - start) > n_minute:
+                break
+            end = trans2ID[cnt + 1][-2]
+            final_trans += trans2ID[cnt + 1][-1]
+            cnt += 1
+        result.append({"show":showhash2name[show], "title":episodehash2name[episode], "snippet":rss_url, "start":start, "end":end, "transcript":final_trans})
+        episode_frequency[episode] += 1
+    return result
+
+def getUrlFromXml(root):
+#     try:
+    item = root[0].find('item')
+    url = item.find('enclosure')
+    return url.attrib['url']
+#     except Exception as e:
+#         print(e)
+
+def rankingTranscript():
+    transcriptLst.sort(key=lambda x: x.score, reverse=True)
+
+def addDocuments(data, score, show, episode, rss_url):
+    start = 0
+    end = 0
+    global idx
+    alternatives = data['results']
+    for alternative in alternatives:
+        transcripts_data = alternative['alternatives']
+        if transcripts_data == [{}]:
+            continue
+        for ele in transcripts_data:
+            if 'transcript' not in ele.keys():
+                continue
+            trans = ele['transcript']
+            start = float(ele['words'][0]['startTime'].rstrip('s'))
+            end = float(ele['words'][-1]['endTime'].rstrip('s'))
+            tmp = transcript(idx, score, trans.split(), trans)
+            # s = tmp.calTFIDF(qr)
+            # if s != 0:
+            transcriptLst.append(tmp)
+            table.add_document(idx, trans.upper().split())
+            trans2ID[idx] = (show, episode, rss_url, start, end, trans)
+            
+            idx += 1
+    # print(table.corpus_dict)
+
+def getRelevantClips(qr, epi_w=0.4):
+    table2 = table.similarities(qr)
+    for i in range(idx):
+        transcriptLst[i].score = epi_w * transcriptLst[i].score + (1 - epi_w) * table2[i][1]
+    # print(table2)
+
+
+# In[110]:
+
+
+
+
+
+# In[111]:
+
+
+#def main():
+    
     
 
 
-# In[36]:
+# In[112]:
+
+api.add_resource(Search, '/Search')
+
+if __name__ == '__main__':
+    app.run()
 
 
-if __name__ == "__main__":
-    main()
 
